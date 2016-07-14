7c66c644b76fa6804070399489e43fffa4820757
Frank Capraro <Frank@Franks-MBP.fios-router.home>
2016-07-13 22:04:56 -0400

    updated the game file to choose a random word.  now i need to make sure that we can check if a word is used in a different file

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
 //Your game.js file will randomly select a word for the play
+
+var wordBank = ['red','yellow','blue','green','orange'];
+
+var solution = function(wordBank){
+	// Choose a random variable from word bank 
+	rand = wordBank[Math.floor(Math.random() * wordBank.length)];
+	return rand;
+}
+
+// return a random work 
+console.log(solution(wordBank));
--- word.js
+++ word.js
@@ -1,1 +1,17 @@
 //word.js should contain all of the methods which will check the letters guessed versus the random word selected.
+
+//first we need to take a value that the user enters and return if a true or false if the work is correct or incorrect 
+// guess is the users guess 
+// answer is the correc answer 
+// alreadyGuessed is an array of the guesses already made 
+// you need to pass in the word 
+var guessCheck = function(guess, answer, guessCheck){
+
+	if(guessCheck !== -1){
+		//return a value to say that this was already guessed and try again
+	}else{
+		// check if the guess is in the answer 
+
+	}
+}
+
